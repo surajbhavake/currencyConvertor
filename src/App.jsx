663a61ca9c2336a85d70eda2f3d9f4b13bbbbcc8
surajbhavake <surajbhavake2@gmail.com>
2026-03-26 01:33:44 +0530

    dark Mode new update

--- src/App.jsx
+++ src/App.jsx
@@ -136,6 +136,7 @@ if(error) return <p>{error}</p>
                     >
                         <div className="w-full mb-1">
                             <InputBox
+                            className={`${darkMode ? "text-black" : "text-white"}`}
                                 label="From"
                                 amount={amount}
                                 currencyOptions={options}
@@ -158,6 +159,7 @@ if(error) return <p>{error}</p>
                         </div>
                         <div className="w-full mt-1 mb-4">
                             <InputBox
+                            className={`${darkMode ? "text-black" : "text-white"}`}
                                 label="To"
     
                                 amount={convertedAmount}
